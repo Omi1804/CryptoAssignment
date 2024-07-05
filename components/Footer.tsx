e3d60501d63a0ac261bea6de972e4cefa17813e4
Om Nigam <om2021064@akgec.ac.in>
2024-07-06 01:35:07 +0530

    adding footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="text-red-500"></div>;
+  return (
+    <div className="border border-[rgba(255,255,255,.15)] my-10 px-[6rem] py-[5rem]">
+      <div className=""></div>
+      <hr />
+      <div></div>
+      <hr />
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import styles from "@/styles/headers.module.css";
 
 const Navbar = () => {
   return (
-    <nav className="border top-0 left-0 w-full px-[5rem] flex items-center">
+    <nav
+      className={` top-0 left-0 w-full px-[5.5rem] flex items-center border-b border-b-[rgba(255,255,255,.15)] justify-center ${styles.navbar}`}
+    >
       <div className=" w-[7rem] h-auto object-contain">
         <img
           src="/svgs/logo.svg"
@@ -13,33 +16,35 @@ const Navbar = () => {
 
       <div className="mx-6">
         <ul className="flex justify-center items-cente">
-          <li className="mx-5 text-[#ffffffb3] text-lg font-normal py-7 cursor-pointer">
+          <li className="mx-4 text-[#ffffffb3] text-[1.1rem] font-normal py-[1.3rem] cursor-pointer">
             Compass
           </li>
-          <li className="mx-5 text-[#ffffffb3] text-lg font-normal py-7 cursor-pointer">
+          <li className="mx-4 text-[#ffffffb3] text-[1.1rem] font-normal py-[1.3rem] cursor-pointer">
             Explore
           </li>
-          <li className="mx-5 text-[#ffffffb3] text-lg font-normal py-7 cursor-pointer">
+          <li className="mx-4 text-[#ffffffb3] text-[1.1rem] font-normal py-[1.3rem] cursor-pointer">
             Academy
-            <span className="bg-[#6434E9] rounded-3xl text-white px-2 py-1 text-sm mx-2">
+            <span className="bg-[#6434E9] rounded-lg text-white px-2 py-1 text-sm mx-2">
               New
             </span>
           </li>
-          <li className="mx-5 text-[#ffffffb3] text-lg font-normal py-7 cursor-pointer">
+          <li className="mx-4 text-[#ffffffb3] text-[1.1rem] font-normal py-[1.3rem] cursor-pointer">
             NFTs
           </li>
-          <li className="mx-5 text-[#ffffffb3] text-lg font-normal py-7 cursor-pointer">
+          <li className="mx-4 text-[#ffffffb3] text-[1.1rem] font-normal py-[1.3rem] cursor-pointer">
             For Projects
           </li>
         </ul>
       </div>
 
-      <div className="bg-[#181818] py-3 px-4 flex items-center justify-center rounded-3xl w-[33%] mx-1">
-        <span className="material-symbols-outlined text-[#fff6]">search</span>
+      <div className="bg-[#181818] py-2 px-4 flex items-center justify-center rounded-3xl w-[35%] mx-1 border border-[rgba(255,255,255,.15)]">
+        <span className="material-symbols-outlined text-[#fff6] font-light text-[1.7rem]">
+          search
+        </span>
         <input
           type="text"
-          placeholder="Search for ecosystems, trending quests etc."
-          className="bg-transparent outline-none border-none w-full h-full placeholder:text-[#fff6] mx-3"
+          placeholder="Search for ecosystems, trending quests etc..."
+          className="bg-transparent outline-none border-none w-full h-full placeholder:text-[#fff6] mx-3 text-[.9rem] font-medium tracking-wide"
         />
       </div>
 
@@ -52,7 +57,7 @@ const Navbar = () => {
       </div>
 
       <div>
-        <button className="bg-white text-black rounded-md py-[7px] px-[24px]  text-[17px] font-medium cursor-pointer">
+        <button className="bg-white text-center text-black rounded-md py-[6px] shadow hover:bg-[#dddcdc] px-[24px]  text-[16px] font-medium cursor-pointer transition duration-300">
           Sing In
         </button>
       </div>
